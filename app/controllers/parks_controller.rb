4304277891dd9f48eb8b79d77825ea2ec7661df5
Eileen Chen <ezch09@gmail.com>
2019-06-05 22:59:23 -0400

    can click day or event for calendar

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,7 @@
 class ParksController < ApplicationController
+  require 'date'
+
+
   include ApiHelper
 
   def show
@@ -10,31 +13,45 @@ class ParksController < ApplicationController
     @alerts_caution= @alerts.select{|alert| alert["category"] == "Caution"}
     @alerts_info = @alerts.select{|alert| alert["category"] == "Information"}
     @articles = keyword_query("/articles?parkCode=#{@park_code}&")["data"]
-    @articles = @articles.select{|article| article["listingimage"]["url"] != "" && article["url"] != ""}.take(5)
+    @articles = @articles.select{|article| article["listingimage"]["url"] != "" && article["url"] != ""}.take(3)
     @events = keyword_query("/events?parkCode=#{@park_code}&pagesize=50&")["data"].select{|event| event["title"] != ""}
     @events_reformatted = []
     
-    @events.each_with_index do |event, i| 
-      arr = {}
-      arr["title"] = event["title"]
-      arr["start"] = event["datestart"]
-      arr["end"] = event["dateend"]
-      arr["location"] = event["location"]
-      arr["times"] = event["times"]
-      arr["allDay"] = event["isallday"]
-      arr["description"] = event["description"]
-      if i%2 == 0
-        arr["color"] = '#006400'
-      else 
-        arr["color"] = '#654321'
+    @events.each do |event| 
+
+  
+      if event["isrecurring"] == "true" 
+        arr = {}
+        arr["title"] = event["title"]
+        arr["start"] = event["datestart"]
+        arr["end"] = event["recurrencedateend"]
+        arr["location"] = event["location"]
+        arr["times"] = event["times"]
+        arr["description"] = event["description"]
+        arr["allDay"] = event["isallday"]
+
+        @events_reformatted.push(arr)
+      
+      
+      else
+        event["dates"].each do |date|
+          arr = {}
+          arr["title"] = event["title"]
+          arr["start"] = date
+          arr["end"] = date
+          arr["location"] = event["location"]
+          arr["times"] = event["times"]
+          arr["description"] = event["description"]
+          arr["allDay"] = event["isallday"]
+          @events_reformatted.push(arr)
+        end
       end
-      @events_reformatted.push(arr)
       
     end
 
 
     @news_releases = keyword_query("/newsreleases?parkCode=#{@park_code}&")["data"]
-    @news_releases = @news_releases.select{|nr| nr["image"]["url"] != "" && nr["url"] != ""}.take(5)
+    @news_releases = @news_releases.select{|nr| nr["image"]["url"] != "" && nr["url"] != ""}.take(3)
     
   end
 
